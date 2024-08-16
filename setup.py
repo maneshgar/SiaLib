b72f9b0a0e8ffd7ec1b4eef9b24dab0ac86e358c
Ben <maneshgar.behnam@gmail.com>
2024-08-15 23:00:57 -0700

    version 1.0

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='siamics',
+    version='1.0',
+    packages=['siamics', 'siamics.data', 'siamics.data.gtex', 'siamics.data.geo', 'siamics.models', 'siamics.models.scGPT', 'siamics.utils'],
+
+    description='SiaMics: Genomics deep learning library',
+    author='Behnam (Ben) Maneshgar',
+    author_email='maneshgar.behnam@gmail.com',
+    url='https://github.com/bmaneshgar/SiaMics',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords=['SiaMics', 'Genomics', 'Deep Learning', 'BioTech'],
+    classifiers=[],
+
+    py_modules=['cli'],
+    install_requires=[
+    ],
+    entry_points={
+        'console_scripts': [
+        ]
+    }
+)
--- siamics/data/geo/utils.py
+++ siamics/data/geo/utils.py
@@ -1,6 +1,18 @@
-import GEOparse
+import pandas as pd
 
-def merge_data(gse_files_list):
-    merged_gse = GEOparse.get_GEO(filepath=gse_files_list[0])
-    for gse in gse_files_list[1:]:
-        gse = GEOparse.get_GEO(filepath=gse_files_list[0])
+def merge_data(gse_list, count_only=False):
+    
+    merged_df = pd.read_csv(gse_list[0], sep='\t')
+    total_count = merged_df.shape[1]
+    print(f"0/{len(gse_list)} - Processing file:: {gse_list[0]}")
+    
+    for ind, gse in enumerate(gse_list[1:]):
+        df = pd.read_csv(gse, sep='\t')
+        total_count += df.shape[1]
+        print(f"{ind+1}/{len(gse_list)} - {total_count} - Processing file:: {gse}")
+        if not count_only:
+            merged_df = pd.merge(merged_df, df, on=merged_df.columns[0])
+        else: 
+            merged_df = None
+    return merged_df, total_count
+ 
