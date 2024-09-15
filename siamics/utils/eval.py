24e84da870e530ec1d259203a71018c1c7bcd374
Ben <maneshgar.behnam@gmail.com>
2024-09-15 13:22:12 -0700

    Eval accepts varibale labels

--- siamics/utils/eval.py
+++ siamics/utils/eval.py
@@ -1,20 +1,20 @@
 from sklearn import metrics 
 class Classification: 
 
-    def __init__(self, data, average='weighted'):
+    def __init__(self, data, gt_lbl, pred_lbl, average='weighted'):
         self.average = average
+        self.gt_lbl = gt_lbl
+        self.pred_lbl = pred_lbl
         
         self.update_metrics(data)
 
     def update_metrics(self, data):
-        self.accuracy = metrics.accuracy_score(data['gt'], data['pred'])
-        self.precision = metrics.precision_score(data['gt'], data['pred'], average=self.average)
-        self.precision = metrics.recall_score(data['gt'], data['pred'], average=self.average)
-        self.cm = metrics.confusion_matrix(data['gt'], data['pred'])
-        self.report = metrics.classification_report(data['gt'], data['pred'])
+        self.accuracy   = metrics.accuracy_score       (data[self.gt_lbl], data[self.pred_lbl])
+        self.precision  = metrics.precision_score      (data[self.gt_lbl], data[self.pred_lbl], average=self.average)
+        self.precision  = metrics.recall_score         (data[self.gt_lbl], data[self.pred_lbl], average=self.average)
+        self.cm         = metrics.confusion_matrix     (data[self.gt_lbl], data[self.pred_lbl])
+        self.report     = metrics.classification_report(data[self.gt_lbl], data[self.pred_lbl])
         
     def print(self):
-        # print(f"Accuracy: {self.accuracy}")
-        # print(f"Precision: {self.precision}")
         print(f"Classification Report: \n\n{self.report}")
         print(f"Confusion Matrix: \n{self.cm}")
